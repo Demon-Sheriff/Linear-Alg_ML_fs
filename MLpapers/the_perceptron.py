a847724dd5869e46c29ea49af9fe778f8be29e69
Anant Shukla <anantshukla404@gmail.com>
2024-11-23 19:55:09 +0530

    fixed weight init, activation_fn and y_train typo; still need to implement the predict function

--- MLpapers/the_perceptron.py
+++ MLpapers/the_perceptron.py
@@ -23,24 +23,24 @@ class Perceptron():
         self.threshold = threshold
         
     def activation_fn(self, z):
-        return [1 if value > self.threshold else 0 for value in z]
+        return np.where(z > self.threshold, 1, 0)
         
     def fit(self, X_train, y_train):
         
         n_samples, n_features = X_train.shape
         # start off with random weights and biases
-        self.weights = np.zeros(n_features)
+        self.weights = np.random.rand(n_features) * 0.01
         self.bias = 0 # start off with zero as the bias
         
-        for _ in self.n_iterations:
+        for _ in range(self.n_iterations):
             # apply the weighted sum or the dot product of theta.T X [Calculating the perception]
             z = np.dot(self.weights, X_train) + self.bias 
             
             # process z to the activation function [basically make predictions based on the perception]
-            y_hat = self.activation_fn(z=z)
+            y_hat = self.activation_fn(z)
             
             # update the weights and baises
-            if y_hat != y:
+            if y_hat != y_train:
                 self.weights = self.weights + self.learning_rate * np.dot(X_train.T, (y_train - y_hat))
                 self.bias = self.bias + self.learning_rate * np.sum(y_train - y_hat)
             
