5dea295471b3b4a7871c1309b6775443f5e64755
Anant Shukla <anantshukla404@gmail.com>
2024-11-26 18:59:13 +0530

    Implementing the perceptron paper

---
+++ MLpapers/the_perceptron.py
@@ -0,0 +1,20 @@
+# Implementing the perceptron paper 
+
+"""
+Source of the paper : 
+https://www.ling.upenn.edu/courses/cogs501/Rosenblatt1958.pdf
+
+Dated year 1958, University of Pensylvannia 
+Written by F Rosenblatt
+
+"""
+
+import numpy as np
+import pandas as pd
+
+class Perceptron():
+    
+    def __init__(self):
+        self.bias = 0
+        self.wieghts = 
+        pass
