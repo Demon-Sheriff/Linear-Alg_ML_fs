658ec1d707487cf86300ac83cc3339b45d7626d1
Anant Shukla <anantshukla404@gmail.com>
2024-12-08 19:13:16 +0530

    implemented the fit function without laplace smoothing and optimisations

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -21,32 +21,22 @@ class NaiveBayesSpamClassifier():
         """ d is the length of the vocabulary (which is basically the total number of unique words in our traning sample) """
         n_samples, d = X_train.shape 
         
-        self.class_priors = []*self.n_classes # intialise the class priors array index 0 is the start of the class. classes -> 0, 1, 2, 3, .... c
-        class_count = []*self.n_classes
+        self.class_priors = np.zeros(self.n_classes) # intialise the class priors array index 0 is the start of the class. classes -> 0, 1, 2, 3, .... c
+        class_count = np.zeros(self.n_classes)
         
         # accumulate the counts of each class/label
         for label in y_train:
             self.class_priors[label] += 1
             
-        for idx,class_prior in enumerate(self.class_priors):
-            class_count[idx] = class_prior
-            class_prior /= n_samples
+        for idx,count in enumerate(self.class_priors):
+            class_count[idx] = count
+            self.class_priors[idx] /= n_samples
             
         # calculate the feature likelihoods for each class
-        self.feature_likelihoods = [[] for _ in range(self.n_classes)]*d
-        
-        # for xij in range(d):
-        #     for class_ in range(self.n_classes):
-        #         # frequency of xij for the current class
-                
-        #         idxs = []
-                
-        #         freq_xij = 
-        #         self.feature_likelihoods[xij][class_] = 1/class_count[class_]
+        self.feature_likelihoods = np.zeros((d, self.n_classes))
         
         for class_ in range(self.n_classes):
-            for word in range(d):
-                for i in range(n_samples):
-                    self.feature_likelihoods[word][class_] += X_train[i][word] * (y_train[i] == class_)
-                
-            self.feature_likelihoods[word][class_] /= class_count[class_]
+            class_indices = np.where(y_train == class_)[0]
+
+            self.feature_likelihoods[:, label] = np.sum(X_train[class_indices], axis=0)
+            self.feature_likelihoods[:, label] = self.feature_likelihoods[:, label] / class_count[label]
