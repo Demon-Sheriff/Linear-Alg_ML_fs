3286e8cb199be23f2945a90329bea8e0235fd81c
Anant Shukla <anantshukla404@gmail.com>
2024-12-12 22:47:32 +0530

    implementing softmax regression

---
+++ softmax-regression.py
@@ -0,0 +1,23 @@
+"""
+    Implementing softmax regression from scratch
+"""
+
+class softmax_regression():
+    
+    def __init__(self, learning_rate=0.01, n_iterations=1000):
+        self.weights = None
+        self.bias = None
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        
+    def softmax_fn(self):
+        pass
+        
+    def fit(self, X_train, y_train):
+        pass
+    
+    def predict(self, X_test):
+        pass
+    
+    def evaluate(self, y_test):
+        pass
